8d5fea4e17953ace52fdbead7a8912343fa4f2e8
Lennert Bontinck <lennertbontinck@live.be>
2018-11-06 12:40:29 +0100

    Initial setup

---
+++ CarMeets/CarMeets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarMeets
+//
+//  Created by Lennert Bontinck on 06/11/2018.
+//  Copyright © 2018 Lennert Bontinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
